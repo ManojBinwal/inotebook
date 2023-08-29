a8e2f3eb2b3e06b3f38132c72170466dfa7c0c11
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-30 01:13:20 +0530

    fixing error in unique email logic

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ const { Router } = require('express');
 connectToMongo();
 
 const app = express()
-const port = 3000
+//free port 3000 for react app
+const port = 5000
 
 app.use(express.json())  //adding middleware
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,33 +1,39 @@
-const express = require("express")
-const router = express.Router()
-const User = require('../models/User')
+const express = require("express");
+const router = express.Router();
+const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
-//creating a User using: POST "/api/auth/"  Doesn't require auth
+// Creating a User using: POST "/api/auth/" Doesn't require auth
 router.post('/', [
     body('email', 'Enter a valid E-mail').isEmail(),
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('password', 'password is too short min 6 characters').isLength({ min: 6 }),
-
-], async(req, res) => {
+], async (req, res) => {
     // Finds the validation errors in this request and returns bad request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    //check whether the user with same email exists already 
-    let user = User.findOne({email: req.body.email})
-    if(user) {
-        return res.status(40).json({error: "sorry a user with this email already exist"})
+    
+    // Check whether the user with the same email exists already
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry, a user with this email already exists" });
+        }
+        
+        // Create a new user
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        });
+        
+        return res.json(user);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal server error" });
     }
-     user = await User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email
-    })
-    // .then(user => res.json(user)).catch(err=> {console.log(err)
-    //     res.json({error: 'please enter a unique value for email' , message: err.message})})
+});
 
-})
-
-module.exports = router
+module.exports = router;
