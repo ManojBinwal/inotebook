5f465a1ce79229e6588e33f1f8fbc05236863d1a
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-29 21:48:39 +0530

    requesting body data and saving user data

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express")
-const router =  express.Router()
+const router = express.Router()
+const User = require('../models/User')
 
-router.get('/', (req, res)=> {
+
+//creating a User using: POST "/api/auth/"  Doesn't require auth
+router.get('/', (req, res) => {
     console.log(req.body)
-    res.send("Hello")
+    const user = User(req.body)
+    user.save()
+    res.send(req.body)
 })
 
 module.exports = router
