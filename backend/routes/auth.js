df47b848d9cf6b14a53c69186043d38798038484
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-30 00:49:05 +0530

    handling custom errors

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,19 +9,24 @@ router.post('/', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('password', 'password is too short min 6 characters').isLength({ min: 6 }),
 
-], (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
+], async(req, res) => {
+    // Finds the validation errors in this request and returns bad request and the error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-
-    User.create({
+    //check whether the user with same email exists already 
+    let user = User.findOne({email: req.body.email})
+    if(user) {
+        return res.status(40).json({error: "sorry a user with this email already exist"})
+    }
+     user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email
-    }).then(user => res.json(user)).catch(err=> {console.log(err)
-        res.json({error: 'please enter a unique value for email' , message: err.message})})
+    })
+    // .then(user => res.json(user)).catch(err=> {console.log(err)
+    //     res.json({error: 'please enter a unique value for email' , message: err.message})})
 
 })
 
