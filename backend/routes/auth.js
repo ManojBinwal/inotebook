caa3ba568f3c70b0e347532e80c850df5d98f7de
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-08-29 22:58:58 +0530

    added express validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const router = express.Router()
 const User = require('../models/User')
-
+const { body, validationResult } = require('express-validator');
 
 //creating a User using: POST "/api/auth/"  Doesn't require auth
-router.post('/', (req, res) => {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/', [
+    body('email', 'Enter a valid E-mail').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('password', 'password is too short min 6 characters').isLength({ min: 6 }),
+
+], (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+    }).then(user => res.json(user));
+
 })
 
 module.exports = router
