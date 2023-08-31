6f9b35ac1b2ea8cfdf650f199e8a2fdd64c92dc8
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-01 04:07:45 +0530

    create basic app / added router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* provide unique key inside component to remount on changing categories */}
+          <Route exact path="/" element=<Home key="home"/> />
+          <Route exact path="/about" element=<About key="about"/> />
+        </Routes>
+
+
+      </BrowserRouter>
+
+    </>
   );
 }
 
