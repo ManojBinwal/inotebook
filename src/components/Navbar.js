99daf0cdcbabc360fe01530f32605181cf86b05e
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-01 23:45:24 +0530

    apply useLocation to set active tab on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link , useLocation} from 'react-router-dom'
+
+
 
 const Navbar = () => {
+
+  let location = useLocation();
+  useEffect(() => {
+    // Google Analytics
+    console.log(location)
+  }, [location]);
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -13,11 +22,11 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className= {`nav-link ${location.pathname==="/" ? "active" : ""}`}  aria-current="page" to="/">Home</Link>
               </li>
 
               <li className="nav-item">
-                <Link className="nav-link " aria-disabled="page" to='/about'>About</Link>
+                <Link className= {`nav-link ${location.pathname==="/about" ? "active" : ""}`} aria-disabled="page" to='/about'>About</Link>
               </li>
             </ul>
             <form className="d-flex" role="search">
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,21 +3,10 @@ import { useState } from "react";
 
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Harry",
-        "class": "5b"
-    }
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Larry",
-                "class": "10b"
-            })
-        }, 1000);
-    }
+    
+
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider value={{  }}>
             {props.children}
         </NoteContext.Provider>
     )
