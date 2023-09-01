44a555e7dcf535967b57b67a5ea78de9680e07c6
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-01 23:25:00 +0530

    exporting function to update state

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/noteContext'
 
 const About = () => {
   const a = useContext(noteContext)
+  useEffect(() => {
+    a.update()
+  }, [])
+  
   return (
     <div>
       this is about {a.state.name} and he is in class {a.state.class}
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,13 +1,23 @@
 import NoteContext from "./noteContext";
 import { useState } from "react";
 
+
 const NoteState = (props) => {
-    const state = {
+    const s1 = {
         "name": "Harry",
         "class": "5b"
     }
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "Larry",
+                "class": "10b"
+            })
+        }, 1000);
+    }
     return (
-        <NoteContext.Provider value={{ state }}>
+        <NoteContext.Provider value={{ state, update }}>
             {props.children}
         </NoteContext.Provider>
     )
