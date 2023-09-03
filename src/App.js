e4f35c2152c461a5f065ddd5c3818ab7e9e80f8d
Manoj Kumar Binwal <114239126+ManojBinwal@users.noreply.github.com>
2023-09-04 05:21:33 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Home'
 import About from './components/About'
 import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -12,12 +14,14 @@ function App() {
       <NoteState>
         <BrowserRouter>
           <Navbar />
-          <Alert message={"this is amazing notes app"}/>
+          <Alert message={"this is amazing notes app"} />
           <div className="container">
             <Routes>
               {/* provide unique key inside component to remount on changing categories */}
               <Route exact path="/" element=<Home key="home" /> />
               <Route exact path="/about" element=<About key="about" /> />
+              <Route exact path="/login" element=<Login key="login" /> />
+              <Route exact path="/signup" element=<Signup key="signup" /> />
             </Routes>
           </div>
 
